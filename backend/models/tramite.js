3ea2c6755e4d6bc956b026d6976f6f2a0d5f7ec7
Ktmich2095 <230881@utxicotepec.edu.mx>
2025-07-15 23:02:24 -0500

    Se inició modulo de tramites del área de Recursos Humanos

---
+++ backend/models/tramite.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const tramiteSchema = new mongoose.Schema({
+  nombre_tramite:
+  {
+    type:String,
+    required:true
+  },
+  requisitos:{
+    type:String,
+    required:true
+  },
+  categoria:{
+    type:String, 
+    required:true
+  }
+});
+
+const Tramite = mongoose.model('Tramite', tramiteSchema);
+export default Tramite;
