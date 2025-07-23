1be9b3399442e058aa82bc1dfe915212571f1818
Ktmich2095 <230881@utxicotepec.edu.mx>
2025-07-23 12:22:44 -0500

    Implementación de Axios

--- mobile/src/modules/quejas-sugerencias/api.js
+++ mobile/src/modules/quejas-sugerencias/api.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 const API = axios.create({
     baseURL: 'http://10.0.25.134:3001/api',
 })
-
 export const fetchTramites = () => API.get('/tramites');
+
+export default API;
