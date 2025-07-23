0b2cdd5cf9b55849c2488f5407c8b51fdf9781eb
Angel Jesus <230592@utxicotepec.edu.mx>
2025-07-23 13:00:11 -0500

    fix
    fix de conflictos para PR

--- mobile/src/modules/quejas-sugerencias/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-    baseURL: 'http://10.0.25.134:3001/api',
-})
-export const fetchTramites = () => API.get('/tramites');
-
-export default API;
