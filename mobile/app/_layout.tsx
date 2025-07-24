1164879591ee98085bd8466388e85809a381d46e
Ktmich2095 <230881@utxicotepec.edu.mx>
2025-07-23 21:48:59 -0500

    Creación de vista principal de la aplicación

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,30 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { Stack } from 'expo-router';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>¡Bienvenido a EmporinOne!</Text>
-      <Text style={styles.text}>Módulo de Quejas y Sugerencias</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Num.Colaborador ej"
-      />
-
-      <Button title="Enviar" onPress={() => alert('Enviado')} />
-    </View>
-  );
+export default function Layout() {
+  return <Stack />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff', padding: 20
-  },
-  text: {
-    fontSize: 18, marginBottom: 10
-  },
-  input: {
-    height: 40, borderColor: '#ccc', borderWidth: 1, width: '100%', marginBottom: 10, paddingHorizontal: 10
-  }
-});
