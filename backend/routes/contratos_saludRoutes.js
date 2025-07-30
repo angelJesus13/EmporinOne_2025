ce008ec606e7700615cb0cfd67f51ffd9064e90f
Angel Jesus <230592@utxicotepec.edu.mx>
2025-07-29 23:03:19 -0500

    fix
    fix en backend

--- backend/routes/contratos_saludRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contratosSaludController = require('../controllers/contratos_saludController');
-
-// Endpoint para obtener el estado del usuario por email
-router.get('/:email', contratosSaludController.obtenerEstadoUsuario);
-
-module.exports = router;
